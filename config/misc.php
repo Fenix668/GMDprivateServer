f80223a5593ec3140da706a6042b82a9947007ad
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-03-24 16:01:01 +0100

    Create misc.php

---
+++ config/misc.php
@@ -0,0 +1,8 @@
+<?php
+// Other settings that might be useful for you :D
+
+$sajukes = true; // aperil foules
+$nalevels = false; // True => levels on newer versions will show up in older ones
+                   // False (default) => Versions that are equal or older than the one of the client will show up. Levels from newer versions won't show up!
+
+$iplogin = false; // Require mail config!. Will send an email if last IP used doesn't match with the ip that logs in!
